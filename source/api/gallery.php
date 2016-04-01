7e058cfb0b5e8e6c48a8d2d0efc0154215748e9b
Tony M <tony@inspecdigital.org>
2016-03-31 17:57:35 -0700

    Add gallery

--- source/api/gallery.php
+++ source/api/gallery.php
@@ -1,5 +1,9 @@
 <?php
-$dir = $_SERVER['DOCUMENT_ROOT'] . '/assets/img/gallery';
+include('../lib/metadata/EXIF.php');
+include('../lib/metadata/Photoshop_File_Info.php');
+$file_dir = '/assets/img/gallery/';
+$thumb_dir = '/assets/img/gallery/thumbs/';
+$dir = $_SERVER['DOCUMENT_ROOT'] . $thumb_dir;
 $ret = array();
 
 if(is_dir($dir)) {
@@ -9,7 +13,13 @@ if(is_dir($dir)) {
         continue;
       }
       else {
-        $ret[] = $file;
+        $exif = get_EXIF_JPEG($dir . $file);
+        $xmp = read_XMP_array_from_text(get_XMP_text(get_jpeg_header_data($dir . $file)));
+        $irb = get_Photoshop_IRB(get_jpeg_header_data($dir . $file));
+        $file_info = get_photoshop_file_info($exif, $xmp, $irb);
+        $file_info['thumb'] = $thumb_dir . $file;
+        $file_info['file'] = $file_dir . $file;
+        $ret['gallery'][] = $file_info;
       }
     }
   }
